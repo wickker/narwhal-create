b9f78f433e9ff4deb8df112ef2128b3d4fd2aeb8
wickker <choydianchun@gmail.com>
2020-08-09 23:35:53 +0800

    formatted title of content info div

--- src/components/info-div.js
+++ src/components/info-div.js
@@ -5,8 +5,12 @@ export default class InfoDiv extends React.Component {
   render() {
     return (
       <div className="infoDiv">
-        <div>
-          Info  
+        <div className="infoHeader">
+          <div className="contentTitle">All Teams</div>
+          <div className="itemCount">Showing 65 out of 65 teams</div>
+        </div>
+        <div className="infoContent">
+
         </div>
       </div>
     );

be12ddaf26c34601cb93806d17fc7d69f078a00e
wickker <choydianchun@gmail.com>
2020-08-10 02:22:55 +0800

    completed team cards

--- src/components/team-card.js
+++ src/components/team-card.js
@@ -2,6 +2,8 @@ import React from "react";
 import "../styles.css";
 import DefaultStar from "../svg/star default.svg";
 import ColorStar from "../svg/star active.svg";
+import CampaignIcon from "../svg/icon-conversations-small.svg";
+import LeadsIcon from "../svg/icon-leads-small.svg";
 
 export default class TeamCard extends React.Component {
   isCreated = (createdAt) => {
@@ -34,6 +36,13 @@ export default class TeamCard extends React.Component {
         </div>
         <div className="teamDescription">
           {props.description}
+        </div>
+        <div className="divider"></div>
+        <div className="cardFooter">
+          <img src={CampaignIcon} alt="Campaign"/>
+          <span className="cardFooterText">{props.campaigns_count} Campaigns</span>
+          <img src={LeadsIcon} alt="Leads"/>
+          <span className="cardFooterText">{props.leads_count.toLocaleString()} Leads</span>
         </div>
       </div>
     );

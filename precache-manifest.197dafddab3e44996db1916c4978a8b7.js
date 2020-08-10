32f8d97b6e095737401e4824c74b08b300227734
wickker <choydianchun@gmail.com>
2020-08-10 21:41:15 +0800

    Updates

---
+++ precache-manifest.197dafddab3e44996db1916c4978a8b7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026ed35278195e03e9dddd6bc70e6cf9",
+    "url": "/narwhal-create/index.html"
+  },
+  {
+    "revision": "c4b10c08203d08ccb481",
+    "url": "/narwhal-create/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "cd8d1c4c49f66f8d431c",
+    "url": "/narwhal-create/static/css/main.a277c33d.chunk.css"
+  },
+  {
+    "revision": "c4b10c08203d08ccb481",
+    "url": "/narwhal-create/static/js/2.9dfa51ec.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/narwhal-create/static/js/2.9dfa51ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd8d1c4c49f66f8d431c",
+    "url": "/narwhal-create/static/js/main.1c3d4ea2.chunk.js"
+  },
+  {
+    "revision": "66da29f5cbbd611c660a",
+    "url": "/narwhal-create/static/js/runtime-main.f6ca52a1.js"
+  },
+  {
+    "revision": "4d5cf6a3c2f988c82c51c891ad4a9b54",
+    "url": "/narwhal-create/static/media/Shape.4d5cf6a3.svg"
+  },
+  {
+    "revision": "0c52c3a8a57ae81b5dee19b5b22ad428",
+    "url": "/narwhal-create/static/media/caret down.0c52c3a8.svg"
+  },
+  {
+    "revision": "b825e8cf610c243ed0d9ed7986a5bfb0",
+    "url": "/narwhal-create/static/media/icon-conversations-small.b825e8cf.svg"
+  },
+  {
+    "revision": "175c11b89db829c200e327743c823708",
+    "url": "/narwhal-create/static/media/icon-leads-small.175c11b8.svg"
+  },
+  {
+    "revision": "f3cfd0cbc1fd596ae1f988e54e1c8703",
+    "url": "/narwhal-create/static/media/icon-mail.f3cfd0cb.svg"
+  },
+  {
+    "revision": "7d19281ba4b2ebb22a967176f641792e",
+    "url": "/narwhal-create/static/media/icon-search.7d19281b.svg"
+  },
+  {
+    "revision": "152d7b5cca7201e0a3b46b566cd81d07",
+    "url": "/narwhal-create/static/media/star active.152d7b5c.svg"
+  },
+  {
+    "revision": "7287f4462fc431dad4fe158c2ba382de",
+    "url": "/narwhal-create/static/media/star default.7287f446.svg"
+  },
+  {
+    "revision": "6a5c384b55d305a86f373af42f6f80df",
+    "url": "/narwhal-create/static/media/sw-logo-white.6a5c384b.svg"
+  }
+]);

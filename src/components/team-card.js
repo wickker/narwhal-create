3d6442c318610ebd9c2de8235f30bf19bfa0e6b6
wickker <choydianchun@gmail.com>
2020-08-10 18:51:46 +0800

    added help icon

--- src/components/team-card.js
+++ src/components/team-card.js
@@ -6,22 +6,24 @@ import CampaignIcon from "../svg/icon-conversations-small.svg";
 import LeadsIcon from "../svg/icon-leads-small.svg";
 
 export default class TeamCard extends React.Component {
-  isCreated = (createdAt) => {
-    if (!createdAt) {
+  isCreated = (createdAt, isArchived) => {
+    if (!createdAt && !isArchived) {
       return "";
-    } else {
+    } else if (createdAt && !isArchived) {
       return "Created " + createdAt;
+    } else {
+      return "Archived";
     }
   };
 
   render() {
     const props = this.props;
-    const createdOn = this.isCreated(props.created_at);
+    const createdOn = this.isCreated(props.created_at, props.is_archived);
     const star = props.is_favorited ? ColorStar : DefaultStar;
-    
+    const color = props.is_archived ? "#EBEEF2" : "white";
 
     return (
-      <div className="teamCard">
+      <div className="teamCard" style={{ backgroundColor: color }}>
         <div className="cardHeader">
           <div>
             <img className="teamImage" alt="Team logo" src={props.image} />
@@ -34,15 +36,17 @@ export default class TeamCard extends React.Component {
             <img alt="Star" src={star} />
           </div>
         </div>
-        <div className="teamDescription">
-          {props.description}
-        </div>
+        <div className="teamDescription">{props.description}</div>
         <div className="divider"></div>
         <div className="cardFooter">
-          <img src={CampaignIcon} alt="Campaign"/>
-          <span className="cardFooterText">{props.campaigns_count} Campaigns</span>
-          <img src={LeadsIcon} alt="Leads"/>
-          <span className="cardFooterText">{props.leads_count.toLocaleString()} Leads</span>
+          <img src={CampaignIcon} alt="Campaign" />
+          <span className="cardFooterText">
+            {props.campaigns_count} Campaigns
+          </span>
+          <img src={LeadsIcon} alt="Leads" />
+          <span className="cardFooterText">
+            {props.leads_count.toLocaleString()} Leads
+          </span>
         </div>
       </div>
     );
